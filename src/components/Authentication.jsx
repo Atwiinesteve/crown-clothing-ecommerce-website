3c69b7837a6d697abce4662966d905bc666c57be
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-02-19 08:57:10 +0000

    updated code - node version from 19 to 16

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { SignInForm } from "../components/SignInForm";
+import { SignInForm } from "../components/SigninForm";
 import { Signup } from "../components/Signup";
 
 import '../styles/Authentication.css';
