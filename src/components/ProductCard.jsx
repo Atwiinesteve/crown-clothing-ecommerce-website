99dde2a526a1188f369846d316e99ebe3e9b85d2
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-06-06 16:51:50 +0300

    products page setup

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-import Button from '../components/Button';
+import Button from "../components/Button";
 
 import "../styles/ProductCard.css";
 
 const ProductCard = ({ product }) => {
-    const [name, price, imageUrl] = product;
-    return (
-      <div className='product-card-container'>
-        <img src={imageUrl} alt={`${name}`} />
-        <div className='footer'>
-          <span className='name'>{name}</span>
-          <span className='price'>{price}</span>
-        </div>
-        <Button buttonType='inverted'>Add to card</Button>
-      </div>
-    );
-  };
-  
-  export default ProductCard;
+	const { name, price, imageUrl } = product;
+	return (
+		<div className="product-card-container">
+			<img src={imageUrl} alt={`${name}`} />
+			<div className="footer">
+				<span className="name">{name}</span>
+				<span className="price">{price}</span>
+			</div>
+			<Button buttonType="inverted">Add to card</Button>
+		</div>
+	);
+};
+
+export default ProductCard;
