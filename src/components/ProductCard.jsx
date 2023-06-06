4d8ef2182fb2bc4162e2af825cf7dea9fc2bb1d8
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-06-06 11:52:45 +0000

    corrected button error

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,20 +4,18 @@ import Button from '../components/Button';
 
 import "../styles/ProductCard.css";
 
-export default function ProductCard({ product }) {
-
-    // product details
-    const { price, name, imageUrl } = product;
+const ProductCard = ({ product }) => {
+    const [name, price, imageUrl] = product;
     return (
-        <>
-            <div className="product-card-container">
-                <img src={imageUrl} alt={`${name}`} />
-                <div className="footer">
-                    <span className="name">{name}</span>
-                    <span className="price">{price}</span>
-                </div>
-                <Button buttonType="inverted">Add to Cart</Button>
-            </div>
-        </>
-    )
-}
+      <div className='product-card-container'>
+        <img src={imageUrl} alt={`${name}`} />
+        <div className='footer'>
+          <span className='name'>{name}</span>
+          <span className='price'>{price}</span>
+        </div>
+        <Button buttonType='inverted'>Add to card</Button>
+      </div>
+    );
+  };
+  
+  export default ProductCard;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -10,7 +10,7 @@ import {
 // importing styles
 import "../styles/Signup.css";
 import { FormInput } from "./FormInput";
-import { Button } from "./Button";
+import  Button  from "./Button";
 import { UserContext } from "../context/user.context";
 
 // default form values
