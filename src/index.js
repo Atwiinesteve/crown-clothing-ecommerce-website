3170ff5c6c32d4bf7d98a528e1d733bf0547d485
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-07-08 17:59:09 +0300

    added redux-toolkit and react-redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import {UserProvider} from './context/user.context';
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 import './index.css';
 import App from './App';
 import { CategoriesProvider } from './context/categories.context';
@@ -10,14 +13,16 @@ import { CartProvider } from './context/cart.context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
