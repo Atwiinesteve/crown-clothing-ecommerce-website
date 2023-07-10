6091ff84e02ab28d235e0e6e55a4733729e8804c
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-07-10 14:23:02 +0300

    completed stripe intergration and accepted payments

--- src/components/StripePaymentForm.jsx
+++ src/components/StripePaymentForm.jsx
@@ -9,18 +9,38 @@ import { PaymentContainer ,FormContainer } from '../styles/StripePaymentForm';
 export default function StripePaymentForm() {
 
     const stripe = useStripe();
-    const elements = useStripe();
+    const elements = useElements();
 
     // payments handler
-    const paymentsHandler = (e) => {
+    const paymentsHandler = async (e) => {
         e.preventDefault();
         if(!stripe || !elements) return;
-        
+        const response = await fetch("/.netlify/functions/create-payment-intent", {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 100000 })
+        }).then((res) => res.json())
+        const {paymentIntent: { client_secret }} = response;
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Kiiza Stephen'
+                }
+            }
+        });
+        if(paymentResult.error) {
+            alert(paymentResult.error)
+        } else if (paymentResult.paymentIntent.status === "succeeded") {
+            alert("Payment Successful..")
+        }
     };
 
   return (
     <PaymentContainer>
-        <FormContainer>
+        <FormContainer onSubmit={paymentsHandler}>
             <CardElement />
             <button>Pay Now</button>
         </FormContainer>
