b365777fcd71d9ad2cec5e278665851bbc723607
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-07-10 13:51:26 +0300

    added payment intent

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,25 @@
+// imports
+require("dotenv").config();
+const stripe = require("stripe");
+
+// exports
+exports.handler = async(event) => {
+    try {
+        const {amount} = JSON.parse(event.body);
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: 'usd',
+            payment_method_types: ['card'],
+        });
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ paymentIntent })
+        }
+    } catch (error) {
+        console.log({ error });
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error })
+        }
+    }
+};
